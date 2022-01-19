b5246e55266f66c29f5eec23fceacdef6940f966
s0mni <trjmnlc@gmail.com>
2022-01-19 21:22:43 +0200

    Task 4 complete

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,5 +8,28 @@ nurodomas gamintojas ir jo pagaminti modeliai.
 Pastaba: Informacija apie automobilį (brand) (jo kortelė) bei turi turėti 
 bent minimalų stilių;
 -------------------------------------------------------------------------- */
+function main() {
+    const ENDPOINT = 'cars.json';
+    document.addEventListener('DOMContentLoaded', () => {
+        getData(ENDPOINT);
+    });
+}
+main();
 
-const ENDPOINT = 'cars.json';
+function getData(url) {
+    const outEl = document.getElementById('output');
+    fetch(url)
+    .then(response => response.json())
+    .then(res => {
+        res.forEach((post) => {
+            const divEl = document.createElement('div');
+            const brandEl = document.createElement('h1');
+            const modelEl = document.createElement('p');
+            brandEl.textContent = post.brand;
+            modelEl.textContent = post.models;
+            divEl.append(brandEl);
+            divEl.append(modelEl);
+            outEl.append(divEl);
+        })
+    })
+}
