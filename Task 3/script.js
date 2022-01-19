52c8881118b55b9366d404d2c00b0fd59dfba00f
s0mni <trjmnlc@gmail.com>
2022-01-19 19:53:48 +0200

    Task 3 complete

--- Task 3/script.js
+++ Task 3/script.js
@@ -10,4 +10,33 @@ Paspaudus mygtuką "Show users":
 Pastaba: Informacija apie user'į (jo kortelė) bei turi turėti bent minimalų stilių;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'https://api.github.com/users';
+function main() {
+    const ENDPOINT = 'https://api.github.com/users';
+    const btnEl = document.getElementById('btn');
+    const outEl = document.getElementById('output');
+    const messageEl = document.getElementById('message');
+    
+    btnEl.addEventListener('click', _=> {
+        messageEl.remove();
+        getUsers(ENDPOINT, outEl);
+    });
+} 
+main();
+async function getUsers(api, el) {
+    try {
+        const res = await (await fetch(api)).json();
+        
+        res.forEach((user) => {
+            const divEl = document.createElement('div');
+            const url = document.createElement('img');
+            const login = document.createElement('h1');
+            login.textContent = user.login;
+            url.src = user.avatar_url;
+            divEl.append(url);
+            divEl.append(login);
+            el.append(divEl);
+        });
+    } catch (e) {
+        console.log(e)
+    }
+}
