f9cc52b36843e9a39c2754b5539002d88cd2b1ed
s0mni <trjmnlc@gmail.com>
2022-01-19 21:59:16 +0200

    Task 6 complete

--- Task 6/script.js
+++ Task 6/script.js
@@ -19,3 +19,19 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+function getUserAverageAge(people) {
+	let sum = 0;
+	people.forEach((value) => {
+		sum += value.age;
+	});
+	console.log(Math.round(sum / people.length));
+}
+getUserAverageAge(users);
+
+
+function getUsersNames(humans) {
+  const newArr = humans.map((human) => human.name);
+  console.log(newArr);
+}
+getUsersNames(users);
